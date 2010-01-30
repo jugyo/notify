d04f314906b704f8aeb7341d1e7ec538d3add1bd
jugyo <jugyo.org@gmail.com>
2010-01-30 21:29:40 +0900

    ignore RUBY_PLATFORM

--- lib/notify/notify-send.rb
+++ lib/notify/notify-send.rb
@@ -1,4 +1,4 @@
-if RUBY_PLATFORM.downcase =~ /linux/ && system('which notify-send 2>&1 > /dev/null')
+if system('which notify-send 2>&1 > /dev/null')
   module Notify
     def self.notify(title, message)
       system 'notify-send', title, message
