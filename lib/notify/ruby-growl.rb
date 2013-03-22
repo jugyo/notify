120524112a8bc29112c7b6db86414949d98d14b2
Kristian Freeman <kristian@kristianfreeman.com>
2013-03-22 14:27:41 -0700

    Add 'notify' notification to work with ruby-growl 4.0

--- lib/notify/ruby-growl.rb
+++ lib/notify/ruby-growl.rb
@@ -2,7 +2,8 @@ module Notify
   begin
     require 'ruby-growl'
 
-    @@growl = Growl.new 'localhost', 'ruby', 'GNTP'
+    @@growl = Growl.new 'localhost', 'ruby'
+    @@growl.add_notification 'notify'
     def self.notify(title, message, option = {})
       @@growl.notify 'notify', title, message, option[:priority] || 0, option[:sticky] || false
     end
