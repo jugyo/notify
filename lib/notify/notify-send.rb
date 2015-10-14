f8fc562f525df65f69332425fa2d4d565c614cb2
Nick <nick@hurtig.ninja>
2015-10-14 21:58:36 +0200

    Added additional options for notify-send

--- lib/notify/notify-send.rb
+++ lib/notify/notify-send.rb
@@ -1,8 +1,14 @@
 module Notify
   if which('notify-send')
     def self.notify(title, message, option = {})
-      iconargs = option.key?(:icon) ? ["-i", option[:icon]] : []
-      system 'notify-send', title, html_escape(message), *iconargs
+      args = []
+      args << "-i" << option[:icon] if option.key?(:icon)
+      args << "-t" << option[:time].to_s if option.key?(:time)
+      args << "-a" << option[:app_name].to_s if option.key?(:app_name)
+      args << "-u" << option[:urgency].to_s if option.key?(:urgency)
+      args << "-c" << option[:category].to_s if option.key?(:category)
+      args << "-h" << option[:hint].to_s if option.key?(:hint)      
+      system 'notify-send', title, html_escape(message), *args
     end
   end
 end
